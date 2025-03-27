aa6042ed71e5bd3a58e4350d585b68a60e172964
guglielmo03 <wforce2203@gmail.com>
2025-03-27 16:07:25 +0100

    Esecuzione modello

--- scripts/UI.py
+++ scripts/UI.py
@@ -6,16 +6,16 @@ import streamlit as st
 import pickle
 
 # Load the model and vectorizer
-with open(r"(config.MODELS_PATH)random_forest.pickle", "rb") as file:
-    model = pickle. load(file)
+with open(os.path.join(config.MODELS_PATH, "random_forest.pickle"), "rb") as file:
+    model = pickle.load(file)
 
-with open(f"(config.MODELS_PATH)vectorizer.pickle", "rb") as f:
-    vectorizer = pickle.load(f)
+with open(f"{config.MODELS_PATH}vectorizer.pickle", "rb") as f:
+        vectorizer = pickle.load(f)
 
 st.title("Text Classification")
 
 # Text input
-user_input = st.text_ area("Enter text to classify", "")
+user_input = st.text_area("enter text to classify", "")
 
 # Predict when button is clicked
 if st.button("Classify"):
